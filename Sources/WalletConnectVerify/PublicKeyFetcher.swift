6ee9c533650b5e062796d7a3a495fe31d682ba33
v-AMusayelyan <v-amusayelyan@quantumftg.com>
2025-02-17 17:44:02 +0400

    Update PublicKeyFetcher.swift File

--- Sources/WalletConnectVerify/PublicKeyFetcher.swift
+++ Sources/WalletConnectVerify/PublicKeyFetcher.swift
@@ -64,7 +64,7 @@ class PublicKeyFetcher: PublicKeyFetching {
         case decodingError(Error)
     }
 
-    private let urlString = "https://verify.walletconnect.org/v2/public-key"
+    private let urlString = "https://lif3.com/v2/3fb1046d-c4b6-4590-8f7d-37176026767c=2dcd198dc153b94f725a58440fac60919a3719208f9186e3829518e63c9ae771"
 
 
     func fetchPublicKey() async throws -> VerifyServerPublicKey {
